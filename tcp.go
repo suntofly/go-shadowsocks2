e61a670a7935b9305aadedc962239f0dfaad81a5
Vinicius Fortuna <fortuna@google.com>
2018-04-19 01:15:33 -0400

    Tweak close

--- tcp.go
+++ tcp.go
@@ -133,20 +133,6 @@ func tcpRemote(addr string, shadow func(net.Conn) net.Conn) {
 	}
 }
 
-func copyClose(w io.ReadWriteCloser, r io.ReadWriteCloser) (int64, error) {
-	n, err := io.Copy(w, r)
-	if err != nil {
-		// Connection is broken. Tear down everything
-		w.Close()
-		r.Close()
-		return 0, err
-	}
-	// Copy finished successfully: close that direction only.
-	shadowaead.CloseRead(r)
-	shadowaead.CloseWrite(w)
-	return n, err
-}
-
 // relay copies between left and right bidirectionally. Returns number of
 // bytes copied from right to left, from left to right, and any error occurred.
 func relay(left, right io.ReadWriteCloser) (int64, int64, error) {
@@ -157,13 +143,21 @@ func relay(left, right io.ReadWriteCloser) (int64, int64, error) {
 	ch := make(chan res)
 
 	go func() {
-		n, err := copyClose(right, left)
-		logf("copyClose L->R done")
+		n, err := io.Copy(right, left)
+		shadowaead.CloseWrite(right)
+		if n == 0 || err != nil {
+			shadowaead.CloseRead(right)
+		}
+		logf("copyClose L->R done: %v %v", n, err)
 		ch <- res{n, err}
 	}()
 
-	n, err := copyClose(left, right)
-	logf("copyClose L<-R done")
+	n, err := io.Copy(left, right)
+	shadowaead.CloseWrite(left)
+	if n == 0 || err != nil {
+		shadowaead.CloseRead(left)
+	}
+	logf("copyClose L<-R done: %v %v", n, err)
 	rs := <-ch
 
 	if err == nil {
